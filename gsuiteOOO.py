8fc9b15ef798cc6679c30ba4c8aef3c07d036e18
Jeffrey Vos <41445094+Jeffvos@users.noreply.github.com>
2019-07-15 18:08:13 +0300

    Update gsuiteOOO.py

--- gsuiteOOO.py
+++ gsuiteOOO.py
@@ -2,15 +2,18 @@ from oauth2client.service_account import ServiceAccountCredentials
 from apiclient.discovery import build
 
 SCOPES = ['https://www.googleapis.com/auth/gmail.settings.basic', 'https://mail.google.com/']
+
+# 'keyp12.p12' should be replaced with the path to your generated p12 key
+#'serviceaccount@gserviceaccount.com' should be replaced with the generated service gsuite account
 SERVICE_ACCOUNT_FILE = 'keyp12.p12'
 SERVICE_ACCOUNT_EMAIL='serviceaccount@gserviceaccount.com'
 
-def gstuite(fullname_leaver, user_email):
+def outReply(fullname_leaver, user_email):
     credentials = ServiceAccountCredentials.from_p12_keyfile(
         SERVICE_ACCOUNT_EMAIL,
         SERVICE_ACCOUNT_FILE,
         scopes=SCOPES)
-
+#You can use html for the body of the Out of office reply. 
     outofoffice={
         'enableAutoReply': True,
         'responseBodyHtml': "<p>Hi, I am no longer working at COMPANY.</p>"
