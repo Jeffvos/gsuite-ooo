0c42f7935307013ce02fbc04477463ff30712c60
Jeffrey Vos <jeff.vos@aol.com>
2020-02-17 10:39:48 +0200

    aded sys argv to get input otherwie by input

--- gsuite_ooo.py
+++ gsuite_ooo.py
@@ -1,15 +1,18 @@
 """ Begin for automated ooo reply (Gsuite) """
+import sys
 from oauth2client.service_account import ServiceAccountCredentials
 from apiclient.discovery import build
 
-SCOPES = ['https://www.googleapis.com/auth/gmail.settings.basic', 'https://mail.google.com/']
+SCOPES = ['https://www.googleapis.com/auth/gmail.settings.basic',
+          'https://mail.google.com/']
 
 """'keyp12.p12' should be replaced with the path to your generated p12 key
 'serviceaccount@gserviceaccount.com' should be replaced with the generated service gsuite account"""
 SERVICE_ACCOUNT_FILE = 'keyp12.p12'
 SERVICE_ACCOUNT_EMAIL = 'serviceaccount@gserviceaccount.com'
 
-def out_reply(fullname_leaver, user_email):
+
+def out_reply(name, email):
     ''' gsuiste ooo reply function '''
     credentials = ServiceAccountCredentials.from_p12_keyfile(
         SERVICE_ACCOUNT_EMAIL,
@@ -18,11 +21,23 @@ def out_reply(fullname_leaver, user_email):
     out_of_office = {
         'enableAutoReply': True,
         'responseBodyHtml': "<p>Hi, I am no longer working at COMPANY.</p>"
-                            "<p>Regards,</p> <p>{}</p>".format(fullname_leaver),
+                            "<p>Regards,</p> <p>{}</p>".format(
+                                fullname_leaver),
         'restrictToDomain': False,
-        }
+    }
 
     credentials = credentials.create_delegated(user_email)
     service = build('gmail', 'v1', credentials=credentials)
-    result = service.users().settings().updateVacation(userId='me', body=out_of_office).execute()
+    result = service.users().settings().updateVacation(
+        userId='me', body=out_of_office).execute()
     return result
+
+
+try:
+    sys_args_name = sys.argv[1]
+    sys_args_email = sys.argv[2]
+    out_reply(sys_args_name, sys_args_email)
+except:
+    fullname_leaver = input('Full name leaver: ')
+    email_leaver = input('Email leaver: ')
+    out_reply(fullname_leaver, email_leaver)
